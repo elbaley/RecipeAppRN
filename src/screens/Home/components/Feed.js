8fe9a07aea9a988ce45c953da5dd0b230c9af7ee
elbaley <lebafurkan@outlook.com>
2022-09-26 23:27:24 +0300

    add Recipe component

--- src/screens/Home/components/Feed.js
+++ src/screens/Home/components/Feed.js
@@ -7,29 +7,21 @@ import {
   Image,
 } from 'react-native';
 import React from 'react';
+import {Heart} from 'iconoir-react-native';
+import Recipe from './Recipe';
 
 const Feed = () => {
   function renderRecipe() {
     return <Text> 12</Text>;
   }
   return (
-    <SafeAreaView>
+    <SafeAreaView style={styles.container}>
       <View style={styles.headingContainer}>
         <Text style={styles.heading}>What do you want to cook today?</Text>
       </View>
       {/* <FlatList data={[1, 2, 3]} renderItem={renderRecipe} /> */}
-      <View>
-        <View style={styles.productContainer}>
-          <View style={styles.productImageContainer}>
-            <Image
-              style={styles.productImage}
-              source={{
-                uri: 'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F44%2F2018%2F07%2F07%2F8306980.jpg',
-              }}
-            />
-          </View>
-        </View>
-      </View>
+      <Recipe />
+      <Recipe />
     </SafeAreaView>
   );
 };
@@ -37,6 +29,10 @@ const Feed = () => {
 export default Feed;
 
 const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+    flex: 1,
+  },
   headingContainer: {
     width: '90%',
     marginHorizontal: '5%',
@@ -45,16 +41,5 @@ const styles = StyleSheet.create({
     fontSize: 28,
     fontFamily: 'Poppins',
     fontWeight: '500',
-  },
-  productContainer: {
-    marginHorizontal: '5%',
-    backgroundColor: 'green',
-    alignItems: 'center',
-  },
-  productImage: {
-    height: 300,
-    width: '100%',
-    aspectRatio: 1,
-    resizeMode: 'cover',
   },
 });
---
+++ src/screens/Home/components/Recipe.js
@@ -0,0 +1,68 @@
+import {StyleSheet, Text, View, Image} from 'react-native';
+import React from 'react';
+import {Heart} from 'iconoir-react-native';
+
+const Recipe = () => {
+  return (
+    <View style={styles.productContainer}>
+      <Image
+        style={styles.productImage}
+        source={{
+          uri: 'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F19%2F2005%2F07%2F25%2Fhot-cold-chicken-spinach-salad-su-2000.jpg',
+        }}
+      />
+      <View style={styles.productInfoContainer}>
+        <View style={styles.leftInfo}>
+          <Text style={styles.title}>Spinach Salad</Text>
+          <Text style={styles.calories}>165 cal • 15 min</Text>
+        </View>
+        <View>
+          <Heart
+            style={styles.heartIcon}
+            height={30}
+            width={30}
+            color="currentColor"
+            strokeWidth="0"
+            stroke="#fff"
+            fill={'#e94a4b'}
+          />
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default Recipe;
+
+const styles = StyleSheet.create({
+  productContainer: {
+    marginHorizontal: '5%',
+    marginTop: 10,
+  },
+  productImage: {
+    height: 200,
+    width: '100%',
+    resizeMode: 'cover',
+    borderRadius: 10,
+  },
+  productInfoContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 10,
+  },
+  title: {
+    fontFamily: 'Poppins',
+    fontWeight: '600',
+    fontSize: 24,
+  },
+  calories: {
+    fontFamily: 'Poppins',
+    fontWeight: '400',
+    color: 'grey',
+    fontSize: 16,
+  },
+  heartIcon: {
+    // backgroundColor: 'red',
+    color: '#c0c4cb',
+  },
+});
